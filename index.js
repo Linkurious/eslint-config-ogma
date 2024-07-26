b5f19faecdb0894e784fbd3fa704debba6d454ce
w8r <alex@milevski.co>
2024-07-26 11:43:21 +0200

    Added promise plugin

--- index.js
+++ index.js
@@ -3,60 +3,61 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true
+    node: true,
   },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:import/recommended',
-    'plugin:import/typescript',
-    'prettier'
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:promise/recommended",
+    "prettier",
   ],
   parserOptions: {
-    ecmaVersion: 2021
+    ecmaVersion: 2021,
   },
-  plugins: ['@typescript-eslint', 'prettier'],
+  plugins: ["@typescript-eslint", "prettier", "promise"],
   rules: {
-    'no-console': 2,
-    '@typescript-eslint/ban-ts-comment': 1,
-    '@typescript-eslint/no-non-null-assertion': 0,
-    'import/order': [
+    "no-console": 2,
+    "@typescript-eslint/ban-ts-comment": 1,
+    "@typescript-eslint/no-non-null-assertion": 0,
+    "import/order": [
       1,
       {
         groups: [
-          'external',
-          'builtin',
-          'internal',
-          'sibling',
-          'parent',
-          'index'
+          "external",
+          "builtin",
+          "internal",
+          "sibling",
+          "parent",
+          "index",
         ],
         pathGroups: [
           {
-            pattern: 'components',
-            group: 'internal'
+            pattern: "components",
+            group: "internal",
           },
           {
-            pattern: 'common',
-            group: 'internal'
+            pattern: "common",
+            group: "internal",
           },
           {
-            pattern: 'routes/**',
-            group: 'internal'
+            pattern: "routes/**",
+            group: "internal",
           },
           {
-            pattern: 'assets/**',
-            group: 'internal',
-            position: 'after'
-          }
+            pattern: "assets/**",
+            group: "internal",
+            position: "after",
+          },
         ],
-        pathGroupsExcludedImportTypes: ['internal'],
+        pathGroupsExcludedImportTypes: ["internal"],
         alphabetize: {
-          order: 'asc',
-          caseInsensitive: true
-        }
-      }
-    ]
-  }
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
 };
